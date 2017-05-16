a7f94e2af881d57a932dabe74cd5ceb05263c56c
Oleg Yunin <oayunin@avito.ru>
2017-05-16 11:50:06 +0300

    fabric load_artifacts task

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,10 @@
 from __future__ import print_function
 
+import glob
+import importlib
 import os
+import sys
+from collections import namedtuple
 from functools import wraps
 
 from fabric import api
@@ -8,13 +12,30 @@ from fabric.colors import green, red
 from fabric.context_managers import cd
 from fabric.decorators import task
 
+from fabric_utils import artifactory
 from fabric_utils import local
 from fabric_utils.rabbit import manage_rabbitmq, wait_rabbit_for_start
 
 
-GIT_ROOT = '/var/local/service'
+GIT_ROOT = os.getenv('GIT_ROOT', '/var/local/service')
+DATA_PATH = os.path.join(GIT_ROOT, 'data')
+
+sys.path.insert(0, GIT_ROOT)
+
+_LocalPath = namedtuple('LocalPath', ['folder', 'file'])
+
+
+# noinspection PyPep8Naming
+def LocalPath(folder, file=None):
+    return _LocalPath(folder, file)
+
+
 api.env.use_ssh_config = True
 api.env.sudo_user = 'user'
+
+
+def error_print(msg, **kwargs):
+    print(red(msg), **kwargs)
 
 
 def with_cd_to(path):
@@ -29,7 +50,7 @@ def with_cd_to(path):
 
 
 def with_cd_to_git_root(func):
-    return with_cd_to(GIT_ROOT)
+    return with_cd_to(GIT_ROOT)(func)
 
 
 @task
@@ -94,7 +115,7 @@ def force_stop():
 def error():
     count = api.sudo('grep ERROR logs.txt | wc -l')
     msg = 'On {} counted {} errors'.format(api.env.host_string, count)
-    print(red(msg))
+    error_print(msg)
 
 
 @task
@@ -109,4 +130,74 @@ def check():
         print(green(msg))
     else:
         msg = '%s is not working' % current_host
-        print(red(msg))
+        error_print(msg)
+
+
+def _load_vertica_driver():
+    url = '%s/libs/vertica/libverticaodbc.so.7.1.1' % artifactory.ARTIFACTORY_PREFIX
+    folder = os.path.abspath(os.path.join('.', 'data'))
+    file_name = 'libverticaodbc.so.7.1.1'
+    local_path_obj = LocalPath(folder, file_name)
+    (response, exception), = artifactory.load_artifacts([(url, local_path_obj)])
+    if exception or response.status_code != 200:
+        msg = 'Vertica driver download failed! %s' % ' '.join([str(response), exception or ''])
+        error_print(msg)
+
+
+def _build_worker_to_models_mapping():
+    packages = glob.glob(os.path.join('service', '*', 'models'))
+    worker_to_models_mapping = {}
+    for path in packages:
+        _, worker_name, _ = path.rsplit('/', 2)
+        module = importlib.import_module(path.replace('/', '.'))
+        worker_to_models_mapping[worker_name] = module.models
+
+    return worker_to_models_mapping
+
+
+def _collect_tasks_for_models_loading():
+    worker_to_models_mapping = _build_worker_to_models_mapping()
+    tasks = []
+    for worker, models_registry in worker_to_models_mapping.items():
+        for model_name, tag in models_registry.tags.items():
+            cls = models_registry[model_name]
+            url = '{prefix}/models/{worker}/{model}-{tag}.tar.gz'.format(
+                prefix=artifactory.ARTIFACTORY_PREFIX,
+                worker=worker,
+                model=cls.model_src_name,
+                tag=tag,
+            )
+            dst_folder = os.path.join(DATA_PATH, worker, 'models', cls.model_src_name)
+
+            local_path_obj = LocalPath(dst_folder)
+            tasks.append((url, local_path_obj))
+
+    return tasks
+
+
+def _load_models_data():
+    tasks = _collect_tasks_for_models_loading()
+    task_results = artifactory.load_artifacts(tasks)
+    failed_results = filter(lambda (resp, exc): exc or resp.status_code != 200, task_results)
+    if failed_results:
+        _msg = lambda r, e: 'Download {.url} failed with {details}'.format(r, details=' '.join([str(r), e or '']))
+        msg = '\n'.join([_msg(*result) for result in failed_results])
+        error_print(msg)
+
+
+def _load_features():
+    url = '%s/features/Data-latest.tar.gz' % artifactory.ARTIFACTORY_PREFIX
+    folder = os.path.abspath(os.path.join('.', 'data', 'Data'))
+    local_path_obj = LocalPath(folder)
+    (response, exception), = artifactory.load_artifacts([(url, local_path_obj)])
+    if exception or response.status_code != 200:
+        msg = '"Data/" download failed! %s' % ' '.join([str(response), exception or ''])
+        error_print(msg)
+
+
+@task
+@with_cd_to_git_root
+def load_artifacts():
+    _load_vertica_driver()
+    _load_features()
+    _load_models_data()
---
+++ fabric_utils/artifactory.py
@@ -0,0 +1,82 @@
+import os
+import shutil
+import tarfile
+from StringIO import StringIO
+from multiprocessing import cpu_count
+from multiprocessing.dummy import Pool as ThreadPool
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+from default_settings import artifactory as artifactory_settings
+
+ARTIFACTORY_PREFIX = os.getenv('SERVICE_ARTIFACTORY_PREFIX', 'http://artifactory/service-local')
+
+
+def get_num_threads():
+    default = min(15, int(cpu_count() / 2))
+    return os.getenv('SERVICE_NUM_THREADS', default)
+
+
+def _put(resp, local_path_obj):
+    if resp.status_code != 200:
+        return
+
+    url = resp.url
+
+    try:
+        if '/' in url and '.tar.gz' in url.rsplit('/')[-1]:
+            # tar archive
+            tar = tarfile.open(fileobj=StringIO(resp.content), mode="r:gz")
+            tar.extractall(local_path_obj.folder)
+            tar.close()
+        else:
+            assert local_path_obj.file is not None
+            try:
+                os.makedirs(local_path_obj.folder)
+            except OSError as e:
+                if e.args[0] == 17:
+                    # `already exists` code
+                    pass
+                else:
+                    raise
+            path = os.path.join(local_path_obj.folder, local_path_obj.file)
+            with open(path, 'wb') as f:
+                resp.raw.decode_content = True
+                shutil.copyfileobj(resp.raw, f)
+    except Exception as e:
+        return e
+
+
+def _fetch_url(url, local_path_obj):
+    """
+    :param url: 
+    :param dst_folder: 
+    :param file_name: 
+    :return: (response, exception)
+
+    """
+    auth = HTTPBasicAuth(**artifactory_settings['credentials'])
+
+    # url = 'artifactory_prefix/test.tar.gz'
+    # dst_folder = "test_tar_dir"
+
+    try:
+        resp = requests.get(url, auth=auth)
+    except requests.RequestException as e:
+        exception = e
+        resp = requests.models.Response()
+        resp.url = url
+    else:
+        exception = _put(resp, local_path_obj)
+
+    return resp, exception
+
+
+def load_artifacts(url_to_folder_mapping_list):
+    num_threads = get_num_threads()
+
+    pool = ThreadPool(processes=num_threads)
+    responses = pool.map(lambda args: _fetch_url(*args), url_to_folder_mapping_list)
+    return responses
+
