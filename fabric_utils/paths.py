364a116b4526ff10a212a0e86d68aba500172a50
Oleg Yunin <oayunin@avito.ru>
2017-06-02 13:28:16 +0300

    Fix paths

--- fabric_utils/paths.py
+++ fabric_utils/paths.py
@@ -1,5 +1,5 @@
 import os
 
-GIT_ROOT = os.getenv('GIT_ROOT', os.path.abspath('..'))
+GIT_ROOT = os.getenv('GIT_ROOT', os.path.abspath(os.path.join(__file__, '..', '..')))
 ARTIFACTORY_MODEL_TAGS_TABLE_PATH = os.path.join(GIT_ROOT, 'artifactory_model_tags.yml')
 DATA_PATH = os.path.join(GIT_ROOT, 'data')
