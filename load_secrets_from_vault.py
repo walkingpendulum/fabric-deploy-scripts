5ee589e1699723a390512bd115fe88e8f5382ced
Oleg Yunin <oayunin@avito.ru>
2018-02-16 18:17:03 +0300

    Revert "Merge pull request #105 783 to master"

---
+++ load_secrets_from_vault.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+
+import json
+import os
+import sys
+import traceback as tb
+
+from fabric_utils.vault import load_secrets_from_vault
+
+
+def load(secret_name, path=None):
+    path = path or '/var/run/secrets'
+    file_path = os.path.join(path, '%s.json' % secret_name)
+    try:
+        with open(file_path) as f:
+            secrets = json.load(f)
+    except IOError:
+        secrets = load_secrets_from_vault(secret_name=secret_name)
+    except Exception:
+        print(tb.format_exc())
+        secrets = {}
+
+    return secrets
+
+
+if __name__ == '__main__':
+    print(
+        json.dumps(
+            load(sys.argv[1]),
+            indent=4,
+            ensure_ascii=False
+        )
+    )
